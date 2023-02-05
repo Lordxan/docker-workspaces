0b66b009648531864213aebf92ad5f5ad6ac3938
Lordxan <52027483+Lordxan@users.noreply.github.com>
2023-02-05 22:20:23 +0400

    Changes to build process

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-find . -name "dockerfile.*" -print0 | xargs -0 -I {} \
-    sh -c "docker build --tag \$(echo {} | sed -e 's/^.\/.*\.//g')-workspace -f {} --build-arg WORKSPACE_USER=$USER ."
+find . -name "*dockerfile.*" -print0 | xargs -0 -I {} \
+    sh -c "docker build --tag \$(echo {} | xargs basename | sed -e 's/^.*\.//g')-workspace -f {} --build-arg WORKSPACE_USER=$USER ."
