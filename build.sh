32c86f2c2b2db5a7ce0247467ef575aaac1c7b82
Lordxan <52027483+Lordxan@users.noreply.github.com>
2023-02-07 16:22:34 +0400

    Fix issue with build order in gh actions

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-find . -name "*dockerfile.*" -print0 | xargs -0 -I {} \
+find . -name "*dockerfile.*" | sort | xargs -I {} \
     sh -c "docker build --tag \$(echo {} | xargs basename | sed -e 's/^.*\.//g')-workspace -f {} --build-arg WORKSPACE_USER=$USER ."
